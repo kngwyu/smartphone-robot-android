b071b14c73e95a97d33710bdf0119540580bea50
kngwyu <yuji.kngw.80s.revive@gmail.com>
2025-09-11 14:21:24 +0900

    pidQRReceiver

--- apps/pidQRReceiver/src/main/java/jp/oist/abcvlib/pidqrreceiver/MainActivity.java
+++ apps/pidQRReceiver/src/main/java/jp/oist/abcvlib/pidqrreceiver/MainActivity.java
@@ -1,15 +1,19 @@
-package jp.oist.abcvlib.pidqrtransmitter;
+package jp.oist.abcvlib.pidqrreceiver;
 
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -19,6 +23,8 @@ import jp.oist.abcvlib.core.inputs.microcontroller.BatteryData;
 import jp.oist.abcvlib.core.inputs.microcontroller.BatteryDataSubscriber;
 import jp.oist.abcvlib.core.inputs.microcontroller.WheelData;
 import jp.oist.abcvlib.core.inputs.phone.OrientationData;
+import jp.oist.abcvlib.core.inputs.phone.QRCodeData;
+import jp.oist.abcvlib.core.inputs.phone.QRCodeDataSubscriber;
 import jp.oist.abcvlib.tests.BalancePIDController;
 import jp.oist.abcvlib.util.QRCode;
 import jp.oist.abcvlib.util.SerialCommManager;
@@ -32,18 +38,16 @@ import jp.oist.abcvlib.util.UsbSerial;
  *
  * @author Christopher Buckley https://github.com/topherbuckley
  */
-public class MainActivity extends AbcvlibActivity implements SerialReadyListener,
-        BatteryDataSubscriber {
-
+public class MainActivity extends AbcvlibActivity implements SerialReadyListener, QRCodeDataSubscriber, BatteryDataSubscriber {
+    private final static String TAG = "MainActivity";
     private BalancePIDController balancePIDController;
-    private QRCode qrCode;
     // Create your data publisher objects
     PublisherManager publisherManager = new PublisherManager();
     private OrientationData orientationData;
     private WheelData wheelData;
-    private final Handler handler = new Handler(Looper.getMainLooper());
     private Map<String, Double> pidData = new HashMap<>();
-
+    private boolean pidvalueChanged = true;
+    private TextView statusTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Passes Android App information up to parent classes for various usages. Do not modify
@@ -51,41 +55,20 @@ public class MainActivity extends AbcvlibActivity implements SerialReadyListener
         // Setup Android GUI. Point this method to your main activity xml file or corresponding int
         // ID within the R class
         setContentView(R.layout.activity_main);
-
-        pidData.put("setPoint", 6.84);
-        pidData.put("p_tilt", -0.12);
-        pidData.put("d_tilt", 0.006);
-    }
-
-    protected void onStart() {
-        super.onStart();
-        handler.post(checkControllerRunnable);
-    }
-
-    private final Runnable checkControllerRunnable = new Runnable() {
-        @Override
-        public void run() {
-            if (balancePIDController != null) {
-                qrCode = new QRCode(getSupportFragmentManager(), R.id.qrFragmentView);
-            } else {
-                handler.postDelayed(this, 100); // Check again in 100ms
-            }
+        statusTextView = findViewById(R.id.statusText);
+        if (statusTextView != null) {
+            statusTextView.setText("Looking for QR Code...");
         }
-    };
+        pidData.put("setPoint", 3.6);
+        pidData.put("p_tilt", -0.2);
+        pidData.put("d_tilt", 0.01);
+    }
 
     public void buttonClick(View view) {
         Button button = (Button) view;
         if (button.getText().equals("Start")) {
             // Sets initial values rather than wait for slider change
-            if (qrCode != null) {
-                JSONObject jsonObject = new JSONObject(pidData);
-                String jsonString = jsonObject.toString();
-                qrCode.generate(jsonString);
-                try {
-                    balancePIDController.setPID(pidData.get("p_tilt"), 0.0, pidData.get("d_tilt"), pidData.get("setPoint"), 0.0, 0.0, 15.0);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+            if (balancePIDController != null) {
                 button.setText("Stop");
                 balancePIDController.startController();
             }
@@ -97,11 +80,12 @@ public class MainActivity extends AbcvlibActivity implements SerialReadyListener
 
     @Override
     public void onSerialReady(UsbSerial usbSerial) {
-        orientationData = new OrientationData
-                .Builder(this, publisherManager).build();
+        orientationData = new OrientationData.Builder(this, publisherManager).build();
         BatteryData batteryData = new BatteryData.Builder(this, publisherManager).build();
         batteryData.addSubscriber(this);
         wheelData = new WheelData.Builder(this, publisherManager).build();
+        QRCodeData qrCodeData = new QRCodeData.Builder(this, publisherManager, this).build();
+        qrCodeData.addSubscriber(this);
         SerialCommManager serialCommManager = new SerialCommManager(usbSerial, batteryData, wheelData);
         setSerialCommManager(serialCommManager);
         super.onSerialReady(usbSerial);
@@ -113,10 +97,7 @@ public class MainActivity extends AbcvlibActivity implements SerialReadyListener
         publisherManager.startPublishers();
 
         // Create your controller/subscriber
-        balancePIDController = (BalancePIDController) new BalancePIDController(outputs).setInitDelay(0)
-                .setName("BalancePIDController").setThreadCount(1)
-                .setThreadPriority(Thread.NORM_PRIORITY).setTimestep(5)
-                .setTimeUnit(TimeUnit.MILLISECONDS);
+        balancePIDController = (BalancePIDController) new BalancePIDController(outputs).setInitDelay(0).setName("BalancePIDController").setThreadCount(1).setThreadPriority(Thread.NORM_PRIORITY).setTimestep(5).setTimeUnit(TimeUnit.MILLISECONDS);
         // Attach the controller/subscriber to the publishers
         orientationData.addSubscriber(balancePIDController);
         wheelData.addSubscriber(balancePIDController);
@@ -125,6 +106,14 @@ public class MainActivity extends AbcvlibActivity implements SerialReadyListener
     // Main loop for any application extending AbcvlibActivity. This is where you will put your main code
     @Override
     protected void abcvlibMainLoop() {
+        if (pidvalueChanged) {
+            try {
+                balancePIDController.setPID(pidData.get("p_tilt"), 0.0, pidData.get("d_tilt"), pidData.get("setPoint"), 0.0, 0.0, 15.0);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            pidvalueChanged = false;
+        }
         balancePIDController.run();
     }
 
@@ -136,5 +125,31 @@ public class MainActivity extends AbcvlibActivity implements SerialReadyListener
     @Override
     public void onChargerVoltageUpdate(long timestamp, double chargerVoltage, double coilVoltage) {
 
+    }
+
+    public void onQRCodeDetected(String qrDataDecoded) {
+        try {
+            JSONObject jsonObject = new JSONObject(qrDataDecoded);
+            Iterator<String> keys = jsonObject.keys();
+            while (keys.hasNext()) {
+                String key = keys.next();
+                // Check if the value is a number to avoid class cast exceptions
+                if (jsonObject.get(key) instanceof Number) {
+                    double value = jsonObject.getDouble(key);
+                    pidData.put(key, value);
+                    Log.d(TAG, "QR Decoded: Updated " + key + " to " + value);
+                }
+            }
+            pidvalueChanged = true; // Signal that PID values have changed and need to be applied
+            runOnUiThread(() -> {
+                if (statusTextView != null) {
+                    statusTextView.setText("QR code found!");
+                }
+            });
+        } catch (JSONException e) {
+            // If the qrDataDecoded is not a valid JSON string, a JSONException will be thrown.
+            // In that case, we do nothing and ignore the QR code.
+            Log.d(TAG, "Invalid QR code value");
+        }
     }
 }
